0ee80c76c1a5371edd0658fe238e6ee84e9882b2
plusvic@gmail.com <plusvic@gmail.com@f9279f4c-8c00-11dd-a017-db29c870d619>
2011-08-05 16:18:43 +0000

    Tagging version 1.6.0

---
+++ tags/yara-1.6.0/yara-python/setupwin64.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+             
+setup(  name = "yara-python",
+        version = "1.6",
+        author = "Victor M. Alvarez",
+        author_email = "victor.alvarez@virustotal.com",
+        ext_modules = [ Extension(
+                                    name='yara', 
+                                    sources=['yara-python.c'],
+                                    include_dirs=['../windows/include', '../libyara'],
+                                    extra_objects=['../windows/yara/x64/Release/libyara64.lib','../windows/lib/pcre64.lib']
+                                    )])
+     
+ 
+                                  
